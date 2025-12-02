6c3da3fc0fdbb4ea4c8de4a47d1b9f6092c863bf
Dawens Holtzman PIERRE <dpierre@solutions.ht>
2025-12-02 01:45:16 -0500

    TD01 : Ajouter un composant pour le contenu principal de la page

--- session-001/src/App.jsx
+++ session-001/src/App.jsx
@@ -1,36 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MBDS_Logo from './assets/MBDS_Logo.png'
+
+function Header() {
+  return (
+    <header>
+      <img 
+        src={MBDS_Logo} 
+        alt="Logo MBDS"
+        className="logo"
+      />
+
+      <h1>Introduction à React</h1>
+      <h2>La découverte des premières notions de React</h2>
+    </header>
+  );
+}
+
+function Infos() {
+  return(
+  <>
+    <p>Ici, nous afficherons des informations interessantes :</p>
+  </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      Bonjour Le monde
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Infos/>
     </>
   )
 }
 
-export default App
+export default App;
